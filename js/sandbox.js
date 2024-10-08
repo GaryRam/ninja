5942550dac66124efb81f2630bba9d3057737c9e
GaryRam <126gramirez@frhsd.com>
2024-10-08 14:05:10 -0400

    first commit

---
+++ js/sandbox.js
@@ -0,0 +1,44 @@
+// alert('hello, world');
+// console.log(1);
+// console.log(2);
+// let age = 25;
+// let year = 2019;
+
+
+//log thinhs to console
+// console.log(age, year);
+
+/* age = 30;
+console.log(age);
+
+const points = 100;
+// points = 50;
+console.log(points); 
+
+var score = 75;
+console.log(score); */
+
+
+
+// strings
+console.log('hello, world');
+
+let email = '126gramirez@frhsd.com';
+console.log(email);
+
+
+// strings concatenation
+let firstName = 'Gary';
+let lastName = 'Ramirez';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+
+// getting characters
+
+
+//strings length
+
+
+//string methods
