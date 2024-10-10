812b651c6e4b44c94efb062e23d334ac14383eda
GaryRam <126gramirez@frhsd.com>
2024-10-10 12:52:13 -0400

    ARRAYS

--- js/sandbox.js
+++ js/sandbox.js
@@ -19,7 +19,7 @@ var score = 75;
 console.log(score); */
 
 
-
+/*
 // strings
 console.log('hello, world');
 
@@ -36,9 +36,111 @@ console.log(fullName);
 
 
 // getting characters
-
+console.log(fullName[2]);
 
 //strings length
-
+console.log(fullName.length);
 
 //string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index); */
+
+
+// let email = '126gramirez@frhsd.com';
+
+// let result = email.lastIndexOf('n');
+
+// let result = email.slice(2,5);
+
+// let result = email.substr(4,10);
+
+// let result = email.replace('m', 'w');
+
+// let result = email.replace('n', 'w');
+
+// console.log(result);
+
+// let radius = 10;
+// const pi = 3.14;
+
+//console.log(radius, pi);
+
+// math operators +, -, /, **, %
+
+//console.log(10 / 2);
+//let result = radius % 3;
+//let result = pi * radius**2;
+
+// order of operation - B I M A S
+
+// let result = 5 * (10-3)**2;
+
+// console.log(result);
+
+// let likes = 10;
+
+// likes = likes + 1;
+//likes++;
+//likes--;'
+
+//likes += 10;
+//likes -= 5;
+//likes *= 2;
+// likes /= 2;
+
+//console.log(likes);
+
+// NaN - not a number
+
+// console.log(5 / 'hell0');
+// console.log(5 * 'hello);
+
+// let result = 'the blog has ' + likes + ' Likes';
+// console.log(result);
+
+// template strings
+//const title = 'Best reads of 2019';
+//const author = 'Mario';
+//const likes = 30;
+
+// concantenation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
+
+// template string way
+//let result = `The blog called ${title} by ${author} has ${likes} likes`;
+//console.log(result);
+
+
+// creating html templates
+//let html = `
+//<h2>${title}</h2>
+//<p> ${author}</p>
+//<span>This blog has ${likes} likes</span>`;
+
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,35];
+// console.log(ages[2]);
+
+//let random = ['shuan', 'crystal', 30, 20];
+//console.log(random);
+
+// console.log(ninjas.length);
+
+// array methods
+
+//let result = ninjas.join(',');
+//let result = ninjas.indexOf('chun-li');
+//let result = ninjas.concat(['ken', 'crystal']);
+let result = ninjas.push('ken');
+result = ninjas.pop();
+
+console.log(result);
